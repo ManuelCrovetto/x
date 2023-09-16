85bdf3b92a0e453326b449df5cb89904eaa31ef2
Manuel Crovetto <87724520+ManuelCrovetto@users.noreply.github.com>
2023-09-16 20:21:20 +0200

    Initial Commit

---
+++ X/XApp.swift
@@ -0,0 +1,17 @@
+//
+//  XApp.swift
+//  X
+//
+//  Created by Manuel Crovetto on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
